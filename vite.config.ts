594345d754cdbc83c3818764a9b37140690e892a
ygzhnozdmr <your.email@example.com>
2025-06-26 09:46:30 +0300

    fix: Update file paths for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -14,9 +14,9 @@ export default defineConfig({
         main: './index.html'
       },
       output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
       }
     }
   },
